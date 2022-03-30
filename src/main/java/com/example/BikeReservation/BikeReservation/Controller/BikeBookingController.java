be037daa8baa50de2b6d2c1771a0e73acb7d25ad
Gyanshan <gyanshanranjit8@gmail.com>
2022-03-30 15:16:13 +0545

    readme

--- src/main/java/com/example/BikeReservation/BikeReservation/Controller/BikeBookingController.java
+++ src/main/java/com/example/BikeReservation/BikeReservation/Controller/BikeBookingController.java
@@ -1,11 +1,9 @@
 package com.example.BikeReservation.BikeReservation.Controller;
 
 
+import com.example.BikeReservation.BikeReservation.Entity.SignIn;
 import com.example.BikeReservation.BikeReservation.Service.*;
-import com.example.BikeReservation.BikeReservation.dto.AddAccountRequest;
-import com.example.BikeReservation.BikeReservation.dto.BikeBookingAcknowledgement;
-import com.example.BikeReservation.BikeReservation.dto.BikeReservationRequest;
-import com.example.BikeReservation.BikeReservation.dto.CustomerAccountAcknowledgement;
+import com.example.BikeReservation.BikeReservation.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,22 +16,36 @@ public class BikeBookingController {
     @Autowired
     AddAccountService addAccountService;
     @Autowired
+    BikeDetailService bikeDetailService;
+    @Autowired
     CancelBooking cancelBooking;
     @Autowired
     DeleteAccount deleteAccount;
     @Autowired
     UpdateBooking updateBooking;
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/bikeBookingSystem")
     public BikeBookingAcknowledgement bikeBook(@RequestBody BikeReservationRequest request) throws ParseException {
         return bikeBookingService.bikeBooking(request);
     }
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/bikeBookingSystem/addCustomer/")
     public String addAccount(@RequestBody AddAccountRequest addAccountRequest){
         return addAccountService.addAccount(addAccountRequest);
     }
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/bikeBookingSystem/signin/")
+    public String signIn(@RequestBody SignInRequest signInRequest){
+        return addAccountService.signInPage(signInRequest);
+    }
     @GetMapping("/bikeBookingSystem/getBalance/{email}")
     public CustomerAccountAcknowledgement getBalance(@PathVariable String email){
         return addAccountService.getBalance(email);
+    }
+    @GetMapping("/bikeBookingSystem/getAllCustomer/")
+    public CustomerAccountAcknowledgementList getAllCustomer(){
+        return addAccountService.getAllCustomer();
     }
     @PutMapping("/bikeBookingSystem/addBalance/")
     public CustomerAccountAcknowledgement addBalance(@RequestBody AddAccountRequest addAccountRequest){
@@ -50,5 +62,11 @@ public class BikeBookingController {
     @PutMapping("/bikeBookingSystem/updateReservation/")
     public BikeBookingAcknowledgement updateBooking(@RequestBody BikeReservationRequest request) throws ParseException {
         return updateBooking.updateBooking(request);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/bikeBookingSystem/bikeDetail")
+    public String bikeDetail(@RequestBody BikeDetailRequest bikeDetailRequest) {
+        return bikeDetailService.addBike(bikeDetailRequest);
     }
 }
